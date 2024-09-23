21e52e5168fb57871113a7fdda2207576dc7783c
hagghat <mahach0396@gmail.com>
2024-09-23 20:04:36 +0600

    Add Some Ui design

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,23 +1,40 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { Button } from "@/components/ui/moving-borders";
 
 const HeroSection = () => {
-    return (
-        <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
-           <div className='p-4 relative z-10 w-full text-center'>
-            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Welcome To Master of Music</h1>
-            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-            Master of Music is your ultimate destination for exploring the world of melodies, harmonies, and rhythm. Whether you are a passionate musician, a budding artist, or simply a music lover, we bring you the finest content to deepen your understanding and appreciation of music.
-            </p>
-            <div className="mt-4">
-                <Link href={'/courses'}>
-                  Explore Courses
-                </Link>
-            </div>
-           </div>
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="blue"
+        />
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-20"
+          fill="blue"
+        />
+      <div className="p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Welcome To Master of Music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Master of Music is your ultimate destination for exploring the world
+          of melodies, harmonies, and rhythm. Whether you are a passionate
+          musician, a budding artist, or simply a music lover, we bring you the
+          finest content to deepen your understanding and appreciation of music.
+        </p>
+        <div className="mt-4">
+          <Link href={"/courses"}>
+         <Button borderRadius="1.75rem"
+          className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
+         Explore Courses
+         </Button>
+          </Link>
         </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,24 @@
 import type { Config } from "tailwindcss";
 
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,13 +35,28 @@ const config: Config = {
         },
         foreground: "var(--foreground, #ffffff)",
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity:'1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       backgroundImage: {
         'smart-gradient': "linear-gradient(135deg, #f5f7fa, #c3cfe2)",
         'radial-gradient': "radial-gradient(var(--tw-gradient-stops, #e0f7fa, #80deea))",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 
 export default config;
